3c049a56ab805a487972afd6e675644e32473c4a
DarkKay007 <omarshadow44@gmail.com>
2024-06-07 05:59:27 -0500

    first commit

---
+++ src/page/view-manager/mainDash/listarUsuario.jsx
@@ -0,0 +1,174 @@
+import './style/listarUsuario.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { TbSquareArrowDownFilled } from "react-icons/tb";
+import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode'; 
+
+const ShowUsers = () => {
+  const [usuarios, setUsuarios] = useState([]);
+  const [busqueda, setBusqueda] = useState('');
+  const [error, setError] = useState('');
+  const [eliminarVisible, setEliminarVisible] = useState(null);
+  const [usuarioAEliminar, setUsuarioAEliminar] = useState(null);
+  const [confirmacionVisible, setConfirmacionVisible] = useState(false);
+  const [userRole, setUserRole] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    mostrarUsuarios();
+    const token = Cookies.get('token');
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      setUserRole(decodedToken.rol);
+    }
+  }, []);
+
+  const mostrarUsuarios = async () => {
+    try {
+      const token = Cookies.get('token');
+      const response = await axios.get('https://backendkurogestor.onrender.com/api/usuarios', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUsuarios(response.data);
+      setError(''); 
+    } catch (error) {
+      console.error('Error al obtener usuarios:', error);
+      setError('Error al obtener usuarios');
+    }
+  };
+
+  const buscarUsuario = async () => {
+    try {
+      if (busqueda.trim() === '') {
+        mostrarUsuarios();
+        return;
+      }
+
+      const token = Cookies.get('token');
+      const response = await axios.get(`https://backendkurogestor.onrender.com/api/usuario/${busqueda}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.data.length === 0) {
+        setError('Usuario no encontrado');
+        setUsuarios([]);
+      } else {
+        setUsuarios(response.data);
+        setError('');
+      }
+    } catch (error) {
+      console.error('Error al buscar usuario:', error);
+      setError('Usuario no encontrado');
+    }
+  };
+
+  const handleChange = (event) => {
+    setBusqueda(event.target.value);
+    setError(''); 
+
+    if (event.target.value.trim() === '') {
+      mostrarUsuarios();
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    buscarUsuario();
+  };
+
+  const handleDeleteClick = (usuario) => {
+    if (eliminarVisible === usuario.usuario_id) {
+      setEliminarVisible(null);
+      setUsuarioAEliminar(null);
+    } else {
+      setEliminarVisible(usuario.usuario_id);
+      setUsuarioAEliminar(usuario);
+      setConfirmacionVisible(false); 
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    try {
+      const token = Cookies.get('token');
+      const usuarioLogueado = jwtDecode(token);
+
+      await axios.delete(`https://backendkurogestor.onrender.com/api/usuario/${usuarioAEliminar.usuario_id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      mostrarUsuarios();
+      setEliminarVisible(null);
+      setUsuarioAEliminar(null);
+      setConfirmacionVisible(false);
+
+      if (usuarioAEliminar.usuario_id === usuarioLogueado.usuario_id) {
+        Cookies.remove('token');
+        Cookies.remove('email');
+        navigate('/');
+      }
+    } catch (error) {
+      console.error('Error al eliminar usuario:', error);
+      setError('Error al eliminar usuario');
+    }
+  };
+
+  return (
+    <div className="container-listar-usuario">
+      <form className="search-form-listar-usuario" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={busqueda}
+          onChange={handleChange}
+          placeholder="Buscar usuario por nombre"
+          className="search-input-listar-usuario"
+        />
+        <button type="submit" className="search-button-listar-usuario">Buscar</button>
+      </form>
+
+      <div className="cards-container-listar-usuario">
+        {error && <p className="error-listar-usuario">{error}</p>}
+        {usuarios.length === 0 && !error && <p className="error-listar-usuario">Usuario no encontrado</p>}
+        {usuarios.map((usuario) => (
+          <div key={usuario.usuario_id} className="card-listar-usuario">
+            <div className="card-header">
+              <h3 className="card-title">{usuario.nombre}</h3>
+              {userRole === 'Manager' && ( 
+                <div className="delete-menu">
+                  {eliminarVisible === usuario.usuario_id && (
+                    <div className="delete-menu-options">
+                      <button className="delete-user-button" onClick={() => setConfirmacionVisible(true)}>Eliminar usuario</button>
+                    </div>
+                  )}
+                  <button className="delete-button" onClick={() => handleDeleteClick(usuario)}>
+                    <TbSquareArrowDownFilled />
+                  </button>
+                </div>
+              )}
+            </div>
+            <p className="card-description">{usuario.email}</p>
+            <p className="card-description">{usuario.rol}</p>
+          </div>
+        ))}
+      </div>
+
+      {confirmacionVisible && (
+        <div className="confirmacion-overlay">
+          <div className="confirmacion-box">
+            <p>{`¿Está seguro de que desea eliminar al usuario ${usuarioAEliminar.nombre}?`}</p>
+            <button onClick={handleDeleteUser}>Confirmar</button>
+            <button onClick={() => setConfirmacionVisible(false)}>Cancelar</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ShowUsers;
---
+++ src/page/view-manager/mainDash/proyecto/mostrarProyectos.jsx
@@ -0,0 +1,152 @@
+import "./style/mostrarProyectos.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { deleteProyect } from './eliminarUnProyecto'; 
+import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+
+const ShowProjects = () => {
+  const [proyectos, setProyectos] = useState([]);
+  const [busqueda, setBusqueda] = useState('');
+  const [error, setError] = useState(''); 
+  const [confirmacionVisible, setConfirmacionVisible] = useState(false); 
+  const [proyectoAEliminar, setProyectoAEliminar] = useState(null); 
+  const [userRole, setUserRole] = useState(null); 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    mostrarProyectos();
+    const token = Cookies.get('token');
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      setUserRole(decodedToken.rol);
+    }
+  }, []);
+
+  const mostrarProyectos = async () => {
+    try {
+      const token = Cookies.get('token');
+      const response = await axios.get('https://backendkurogestor.onrender.com/api/proyectos', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setProyectos(response.data);
+      setError('');
+    } catch (error) {
+      console.error('Error al obtener proyectos:', error);
+    }
+  };
+
+  const buscarProyecto = async () => {
+    try {
+      if (busqueda.trim() === '') {
+        mostrarProyectos();
+        return;
+      }
+
+      const token = Cookies.get('token');
+      const response = await axios.get(`https://backendkurogestor.onrender.com/api/proyecto/${busqueda}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.data.length === 0) {
+        setError('Proyecto no encontrado');
+        setProyectos([]);
+      } else {
+        setProyectos(response.data);
+        setError('');
+      }
+    
+    } catch (error) {
+      console.error('Error al buscar proyecto:', error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setBusqueda(event.target.value);
+    setError('');
+
+    if (event.target.value.trim() === '') {
+      mostrarProyectos();
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    buscarProyecto();
+  };
+
+  const handleDelete = async (nombre) => {
+    setProyectoAEliminar(nombre);
+    setConfirmacionVisible(true);
+  };
+
+  const confirmarEliminar = async () => {
+    try {
+      const token = Cookies.get('token');
+      await deleteProyect(proyectoAEliminar, token);
+      setConfirmacionVisible(false);
+      mostrarProyectos();
+    } catch (error) {
+      console.error('Error al eliminar proyecto:', error);
+    }
+  };
+
+  const mostrarTareasProyecto = (proyectoNombre) => {
+    navigate(`/dash-manager/tareas/${proyectoNombre}`);
+  };
+
+  return (
+    <div className="container">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={busqueda}
+          onChange={handleChange}
+          placeholder="Buscar proyecto..."
+          className="search-input"
+        />
+        <button type="submit" className="search-button">Buscar</button>
+      </form>
+
+      {error && <p className="error-message">{error}</p>}
+
+      <div className="cards-container">
+        {proyectos.map((proyecto) => (
+          <div key={proyecto.id} className="card">
+            <h3 className="card-title">{proyecto.nombre}</h3>
+            <p className="card-description">{proyecto.descripcion}</p>
+            <p className="card-info">Estado: {proyecto.estado}</p>
+            <p className="card-info">Prioridad: {proyecto.prioridad}</p>
+            <p className="card-info">Manager del proyecto: {proyecto.manager_email}</p>
+            <p className="card-info">Fecha de actualización: {proyecto.fecha_actualizacion}</p>
+            <button className="task-button" onClick={() => mostrarTareasProyecto(proyecto.nombre)}>
+              Tareas
+            </button>
+            {userRole === 'Manager' && (
+              <button className="delete-button" onClick={() => handleDelete(proyecto.nombre)}>
+                Eliminar
+              </button>
+            )}
+          </div>
+        ))}
+      </div>
+
+      {confirmacionVisible && (
+        <div className="confirmacion-overlay">
+          <div className="confirmacion-box">
+            <p>{`¿Está seguro de que desea eliminar el proyecto ${proyectoAEliminar}?`}</p>
+            <button onClick={confirmarEliminar}>Confirmar</button>
+            <button onClick={() => setConfirmacionVisible(false)}>Cancelar</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ShowProjects;
