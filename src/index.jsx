d05d09e2d28b5a56117a171f85a24286e408c650
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-07 20:50:09 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app.jsx';
 import axios from 'axios';
 
-// Configura los valores predeterminados de Axios aquí
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'https://back-kuro-gestor-1.onrender.com/api';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
